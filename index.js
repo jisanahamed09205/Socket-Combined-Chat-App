5ce5ae2783116dab86e8fca3dbb2b612b7b65e9b
jisanahamed09205 <jisanahamed09205@gmail.com>
2024-04-09 12:15:23 +0600

    real time message transfer for all broadcast message to all connected clients

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const app= express();
 const http = require('http');
 const expressServer = http.createServer(app);
 
+const {Server} = require('socket.io');
+let io = new Server(expressServer)
 
 app.get('/',function (req,res){
     res.sendFile(__dirname+"/index.html")
+})
+
+
+io.on('connection', function(socket){
+    socket.on('chat',function (msg){
+        io.emit('chat_transfer',msg)
+    })
 })
 
 expressServer.listen(5000, function(){
